3ae4369de16b11f6cfb5dba9af2fc39eeca6f636
Peter Crowther <petercrowther90@hotmail.com>
2017-10-10 17:09:44 +0100

    console.rb file created

---
+++ console.rb
@@ -0,0 +1,24 @@
+require("pry-byebug")
+require("pry")
+require_relative("models/bounty_hunter.rb")
+
+bounty_1 = Bounty.new(
+  {
+    name => "Bobo",
+    species => "Space Weegie",
+    bounty_value => "4000",
+    danger_level => "7"
+  }
+)
+
+bounty_2 = Bounty.new(
+  {
+    name => "Squeege",
+    species => "Sponger",
+    bounty_value => "2000",
+    danger_level => "4"
+  }
+)
+
+binding.pry
+nil
