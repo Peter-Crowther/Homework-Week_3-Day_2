7f42d777f63a3b8ef800a5c22390a7a13f17d2f5
Peter Crowther <petercrowther90@hotmail.com>
2017-10-10 17:00:29 +0100

    created bounties table

--- models/bounty_hunter.rb
+++ models/bounty_hunter.rb
@@ -3,12 +3,12 @@ class Bounty
 attr_accessor :name, :species, :bounty_value, :danger_level
 attr_reader :id
 
-def initialize(bounties)
-    @id = bounties["id"].to_i if options["id"]
-    @name = bounties["name"]
-    @species = bounties["species"]
-    @bounty_value = bounties["bounty_value"].to_i()
-    @danger_level = bounties["danger_level"].to_i()
+def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @name = options["name"]
+    @species = options["species"]
+    @bounty_value = options["bounty_value"].to_i()
+    @danger_level = options["danger_level"].to_i()
   end
 
 end
